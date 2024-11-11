32592e5d42c06028f7c6bd74826a993bc645ab24
Siva Chandu Mallala <mallalasivachand@gmail.com>
2024-11-11 19:09:32 +0530

    check 2

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -4,9 +4,9 @@ const profile = new mongoose.Schema({
   email: { type: String, unique: true, require: true },
   name: { type: String, required: true },
   role:{type:String,required:true , default:"student"},
-  rollno:{type:String,default:"0"},
-  department:{type:String,default:"0"},
-  year:{type:String,default:"0"},
+  rollno:{type:String,default:" "},
+  department:{type:String,default:" "},
+  year:{type:String,default:" "},
 },
 {timestamps:true});
 
